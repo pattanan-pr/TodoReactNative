35f54a58a73d92557cd71e2f82450833b8ad62d0
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2023-06-26 15:08:08 +0700

    add file

--- android/app/src/main/java/com/project01/MainActivity.java
+++ android/app/src/main/java/com/project01/MainActivity.java
@@ -1,4 +1,5 @@
 package com.project01;
+
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -16,14 +17,6 @@ public class MainActivity extends ReactActivity {
     return "Project01";
   }
 
-  public class MainActivity extends ReactActivity {
-  // ...
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
-  // ...
-}
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
